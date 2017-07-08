5260214eebeabdd9ad81894f09391521748473ce
Andrzej <andrzej.zuzak@miquido.com>
2017-07-08 13:20:04 +0200

    Adds Gson Converter

--- app/src/main/java/com/example/andrzejzuzak/coroutinesfun/ApiController.kt
+++ app/src/main/java/com/example/andrzejzuzak/coroutinesfun/ApiController.kt
@@ -1,18 +1,29 @@
 package com.example.andrzejzuzak.coroutinesfun
 
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.CoroutineName
 import kotlinx.coroutines.experimental.launch
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiController {
 
     const val POST_COROUTINE_NAME = "post"
 
-    val service: ApiService = Retrofit.Builder()
-            .baseUrl(ApiService.BASE_URL)
-            .build()
-            .create(ApiService::class.java)
+    val service: ApiService
+
+    init {
+        val gson = GsonBuilder()
+                .setLenient()
+                .create()
+
+        service = Retrofit.Builder()
+                .baseUrl(ApiService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(ApiService::class.java)
+    }
 
     fun getPost(postId: Int,
                 success: (PostDto?) -> Unit,
